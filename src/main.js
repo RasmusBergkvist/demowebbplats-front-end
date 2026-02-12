35b8a24924ac62b997401bca11bac5fd1789d209
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-02-12 14:01:02 +0100

    lagt till js för att manuellt byta mörk och ljust tema.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+"use strict";
+
 import './scss/main.scss';
+
+
+const themeButton = document.querySelector("#theme-switch");
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+});
