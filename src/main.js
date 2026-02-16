cdf5f557dab1294cbcfc1205a761193f6c905bf0
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-02-16 15:53:30 +0100

    lagt till extend samt uppdaterat js och mixins gällande tema.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import './scss/main.scss';
 const themeButton = document.querySelector("#theme-switch");
 
 themeButton.addEventListener("click", () => {
-    document.body.classList.toggle("light-theme");
-    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+
+    } else {
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    }
+
 });
