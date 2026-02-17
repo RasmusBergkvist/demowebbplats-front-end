fc19981bd080d676d208d08292b673bb7ab0ca89
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-02-17 16:29:27 +0100

    uppdaterat vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-export default {
-    css: {
-        devSourcemap: true,
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sass: resolve(__dirname, "sass.html")
+      }
     }
-}
+  },
+  css: {
+    devSourcemap: true
+  }
+});
