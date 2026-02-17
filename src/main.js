2212a3a78ac78ab16a010f38c27a0d98a12654a0
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-02-17 15:44:08 +0100

    Uppdaterat layout, samt har skapat local storage för att valt tema inte ska ändras när sidan laddas om.

--- src/main.js
+++ src/main.js
@@ -3,17 +3,31 @@
 import './scss/main.scss';
 
 
-/* Temaknapp */
 const themeButton = document.querySelector("#theme-switch");
 
+/* Hämtar sparat tema från local storage */
+const savedTheme = localStorage.getItem("theme");
+
+/* Om det finns ett sparat tema läggs det på body */
+if (savedTheme) {
+    document.body.classList.add(savedTheme);
+}
+
+
 themeButton.addEventListener("click", () => {
-    if(document.body.classList.contains("dark-theme")){
-        document.body.classList.remove("dark-theme");
+    /* Kollar om mörk tema används */
+    if (document.body.classList.contains("dark-theme")) {
+       
+    /* Tar bort mörkt tema, lägger till ljust tema samt sparat det i local storage */   
+     document.body.classList.remove("dark-theme");
         document.body.classList.add("light-theme");
+        localStorage.setItem("theme", "light-theme");
 
     } else {
+        /* Tar bort ljust tema, lägger till mörkt tema samt sparat det i local storage */   
         document.body.classList.remove("light-theme");
         document.body.classList.add("dark-theme");
+        localStorage.setItem("theme", "dark-theme");
     }
 
 });
