daf8a293dde7f5cb9ad1ce21dae7b6ea1fac30de
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-02-23 10:13:51 +0100

    Start labb 4.

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         sass: resolve(__dirname, "sass.html")
+        animering: resolve(__dirname, "animering.html")
       }
     }
   },
