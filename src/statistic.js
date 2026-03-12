9c5c668968e1c3544302634d5f21cade630e8550
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-03-12 10:53:35 +0100

    Skapat funktion courseChart för att skriva ut stapeldiagram till de populäraste kursena.

---
+++ src/statistic.js
@@ -0,0 +1,91 @@
+"use strict";
+
+import Chart from 'chart.js/auto';
+
+let allData = [];
+
+/**
+ * Hämtar antagningsdata HT2025 för MIUN:s program och kurser.
+ * Datan hämtas asynkront från URL:en med fetch-anrop.
+ * Vid svar omvandlas datan till JSON-format och lagras i arrayen allData.
+ * @error Eventuella fel i anropet fångas upp via catch och skrivs ut i konsolen.
+ */
+
+async function getData() {
+    const url = "https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json";
+
+    try {
+        const response = await fetch(url);
+        allData = await response.json();
+        return allData;
+
+
+    } catch (error) {
+        console.error("Felmeddelande: ", error);
+    }
+}
+
+
+/**
+ * getCourses
+ * 
+ * Funktionen filtrerar ut endast kurser, omvandlar antalet sökande till nummer,
+ * sorterar kurserna efter flest sökande och returnerar de 6 mest populära.
+ * @param {Array} allData - Array med MIUN:s antagningsdata för HT25
+ * @returns {Array<{name: string, applicantsTotal: number}>} - Array med upp till 6 objekt  varje objekt innehåller kursnamn och antal sökande
+ */
+function getCourses(allData) {
+    /* Filterar endast kurser */
+    const courses = allData.filter(course => course.type === "Kurs");
+
+    /* Skapar nytt objekt med kursnamn och antal sökande */
+    const courseList = courses.map(course => ({
+        name: course.name,
+        applicantsTotal: Number(course.applicantsTotal)
+    }));
+
+    /* Sortera flest kurser som har flest sökande */
+    courseList.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+    /* De 6 mest sökta kurserna HT25 */
+    const mostAppliedCourses = courseList.slice(0, 6);
+
+
+    return mostAppliedCourses;
+
+}
+
+async function courseChart() {
+    const allData = await getData();
+    const mostAppliedCourses = getCourses(allData);
+
+    const labels = mostAppliedCourses.map(course => course.name);
+    const data = mostAppliedCourses.map(course => course.applicantsTotal);
+
+
+
+    const ctx = document.getElementById('barChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Antal sökande till de 6 populäraste kurserna',
+                data: data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+}
+courseChart();
+
+export { getData }
